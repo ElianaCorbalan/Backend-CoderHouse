ee5d9a891380dd70a45ed6fccb02110ecec85f61
ElianaCorbalan <eli_corbalan@hotmail.com.ar>
2021-07-21 22:21:41 -0300

    envio de desafio numero 11 con plantillas PUG y EJS

---
+++ 11-Pug-y-EJS/PLANTILLA-PUG/server.js
@@ -0,0 +1,40 @@
+const express=require('express');
+const puerto = 8080;
+const app=express();
+
+app.use('/static', express.static(__dirname + '/public'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.engine('pug', require('pug').__express)
+// Se indica el directorio donde se almacenarán las plantillas 
+app.set('views', './views');
+// Se indica el motor del plantillas a utilizar
+app.set('view engine','pug');
+
+let producto=require('./productos');
+let router = express.Router();
+app.use('/', router)
+
+
+app.get('/',(req,res)=>{
+    res.render('index', {object: []})
+});
+
+// incorporar una nuevo producto
+router.post('/', (req, res) => {
+    producto.nuevoProducto(req.body)
+    res.render('index',{object: producto.listaProductos})
+});
+
+// pongo a escuchar el servidor en el puerto indicado
+const server = app.listen(puerto, () => {
+    console.log(`servidor escuchando en http://localhost:${puerto}`);
+});
+
+// en caso de error, avisar
+server.on('error', error => {
+    console.log('error en el servidor:', error);
+});
+
+
+
